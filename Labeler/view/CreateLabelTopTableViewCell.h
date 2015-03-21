ec9fa6d933bd617ad0cbdfbde2047e8f8c00cd67
tuyo1009 <tuyo1009@gmail.com>
2015-03-21 22:25:30 +0900

    遷移先ページのテーブル設定

--- Labeler/view/CreateLabelTopTableViewCell.h
+++ Labeler/view/CreateLabelTopTableViewCell.h
@@ -1,5 +1,5 @@
 //
-//  CreateLabelViewController.h
+//  CreateLabelTopTableViewCell.h
 //  Labeler
 //
 //  Created by EndoTsuyoshi on 2015/03/21.
@@ -8,8 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CreateLabelViewController : UIViewController
-<UITableViewDelegate,
-UITableViewDataSource>
+@interface CreateLabelTopTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imvTop;
 
 @end
